ef25155dd88c8098e007b9893afb2846d93a6fb8
Kaya Celebi <kayacelebi17@gmail.com>
2024-09-06 16:22:14 -0400

    Filled in Q learning agent

--- src/machine_learning/q_learning_agents.py
+++ src/machine_learning/q_learning_agents.py
@@ -1,3 +1,5 @@
+import random
+
 import sys
 sys.path.append('../')
 
@@ -13,3 +15,56 @@ class QLearningAgent(ReinforcementAgent):
 		ReinforcementAgent.__init__(self, **args)
 
 		self.q_values = util.Counter()?
+
+
+	def get_Q_value(self, state, action):
+		"""
+			Returns Q(state,action)
+			Should return 0.0 if we have never seen a state
+			or the Q node value otherwise
+		"""
+		return self.q_values[(state, action)]
+
+	def compute_value_from_Q_values(self, state):
+		"""
+			Returns max_action Q(state,action)
+			where the max is over legal actions.  Note that if
+			there are no legal actions, which is the case at the
+			terminal state, you should return a value of 0.0.
+		"""
+
+		legal_actions = self.get_legal_actions(state)
+
+		#Terminal
+		if len(legal_actions) == 0:
+			return 0.0
+
+		return max([self.getQValue(state, a) for a in legal_actions])
+
+
+	def compute_action_from_Q_values(self, state):
+		"""
+			Compute the best action to take in a state.  Note that if there
+			are no legal actions, which is the case at the terminal state,
+			you should return None.
+		"""
+		legal_actions = self.get_legal_actions(state)
+
+		# Terminal
+		if len(legal_actions) == 0:
+			return None
+
+		best = sorted([(a, self.get_Q_value(state, a)) for a in legal_actions], key = lambda x: x[1], reverse = True)
+		return random.choice([b[0] for b in best if b[1] == best[0][1]])
+	
+	def get_action(self, state):
+		legal_actions = self.get_legal_actions(state)
+
+		# Terminal state
+		if len(legal_actions) == 0:
+			return None
+
+		if random.random() < self.epsilon:
+			return random.choice(legal_actions)
+		return self.compute_action_from_Q_values(state)
+
