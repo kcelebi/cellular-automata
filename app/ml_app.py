42059496e4635e915e72a257da1633de1fd8e0bd
Kaya Celebi <kayacelebi17@gmail.com>
2024-09-17 02:11:33 -0400

    Adding more func

--- app/ml_app.py
+++ app/ml_app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 import numpy as np
+from markupsafe import escape
 
 import sys
-sys.path.append('../src/')
+sys.path.append('src/')
 
 from path_handler import PathHandler as PH
 import sim.automata as atm
@@ -11,5 +12,17 @@ from sim.rules import Rules
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return "<p>Hello world</p>"
+@app.route('/<view>')
+def home(view = False):
+	return render_template('index.html', view = view, tests = get_tests())
+
+def get_tests():
+	return ["test1", "test2", "test3", "test4"]
+
+@app.route('/result')
+def result():
+	return "Hello hello result."
+
+@app.route('/var_test/<test_test>')
+def view_var_test(test_test):
+	return f'Hello {escape(test_test)}'
